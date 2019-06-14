15e385ecb9dfd34488a05748cd22d9ff1f521076
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2019-06-14 10:37:38 -0300

    Version 10

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Openshift, Version 9
+          Hello Openshift, Version 10
         </p>
         <a
           className="App-link"
